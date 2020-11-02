32afcd2398b105c9eedf7c9d084d0963677e57cb
MattLiu-mygit <mliu78@jh.edu>
2020-11-01 23:23:48 -0500

    Well wishing card skeleton implemented, overall background implemented

---
+++ src/Components/WellWishes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const WellWishes = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: 'aliceblue',
+        width: window.innerWidth / 10,
+        height: window.innerHeight / 10,
+      }}
+    >
+      Wheeeeee
+    </div>
+  );
+};
+
+export default WellWishes;
